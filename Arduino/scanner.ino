73a637dd9bccbc1096d1b43a63a692206efaf09e
tdbxbb <tdbxbb@mst.edu>
2017-11-26 13:46:54 -0600

    Sample output has 2 devices
    I used two devices to make the sample output. Device addresses are 6 sets of two characters folllowed by a space then RSSI value. After that there is a new line

---
+++ Arduino/scanner.ino
@@ -0,0 +1,55 @@
+
+#include <bluefruit.h>
+
+/* For a list of EIR data types see:
+ *    https://www.bluetooth.com/specifications/assigned-numbers/generic-access-profile
+ *    Matching enum: cores/nRF5/SDK/components/softdevice/s132/headers/ble_gap.h */
+
+void setup() 
+{
+  Serial.begin(115200);// baud rate
+
+  Bluefruit.begin(true, true); //enables modes
+ 
+  Bluefruit.setTxPower(4);// max power
+
+  Bluefruit.setName("SD52");
+
+  /* Set the LED interval for blinky pattern on BLUE LED */
+  Bluefruit.setConnLedInterval(1000);
+
+
+  Bluefruit.Scanner.setRxCallback(scan_callback);
+  Bluefruit.Scanner.restartOnDisconnect(true);
+  Bluefruit.Scanner.setInterval(160, 80);       //standard for finding advertisements
+  Bluefruit.Scanner.useActiveScan(true);        
+  Bluefruit.Scanner.start(0);                   //lasts forever
+
+}
+
+void scan_callback(ble_gap_evt_adv_report_t* report)
+{
+
+  
+  //Device address 
+   Serial.printBuffer(report->peer_addr.addr, 6, ':');
+  Serial.print(" ");
+
+ //RSSI Value
+  Serial.printf("%d \n",report->rssi);
+
+  
+
+  
+
+}
+
+
+
+void loop() 
+{
+  
+  digitalToggle(LED_RED);
+
+  delay(1000);
+}
